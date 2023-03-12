d19e9c97d08b84778bcf8bfea856f39d94de1206
Tamas Czirjak <ctamas@users.noreply.github.com>
2023-03-12 15:22:16 +0100

    Search and sorting working

--- src/App.js
+++ src/App.js
@@ -8,29 +8,99 @@ import React from 'react';
 import Card from './components/card'
 
 function App() {
-  const [characters, setCharacters] = React.useState([1]);
+  const [characters, setCharacters] = React.useState([]);
+  const [loading, setLoading] = React.useState(false)
+  const [errorText, setErrorText] = React.useState('')
+  const [characterCount, setCharacterCount] = React.useState(0)
+  const [searchText, setSearchText] = React.useState('')
+  const [sortSelector, setSortSelector] = React.useState('Unsorted')
+  const previousSearch = React.useRef('')
+  const nextPage = React.useRef(1);
 
-  const loadPeople = (page = 1) => {
+  function loadPeople() {
+    setLoading(true);
+    if (previousSearch.current !== searchText) {
+      nextPage.current = 1;
+    }
     // Let chart component know loading is in progress.
-    fetch("https://swapi.dev/api/people/?page=" + page)
+    fetch("https://swapi.dev/api/people/?page=" + nextPage.current + "&search=" + searchText)
       .then(res => res.json())
       .then(
         result => {
-          console.log(result, 'result')
-          setCharacters(result.results)
+          // Only keep the data we need
+          const slimResult = result?.results?.map((result) => {
+            return { name: result.name, gender: result.gender }
+          })
+          console.log('previousSearch.current', previousSearch.current, 'searchText', searchText)
+          if (previousSearch.current !== searchText) {
+            setCharacters(sortResults(slimResult));
+          } else {
+            setCharacters(sortResults([...characters, ...slimResult]));
+          }
+          if (result.next) {
+            nextPage.current = result.next.replace(/\D/g, '');
+          }
+          previousSearch.current = searchText;
+          setCharacterCount(result.count);
+          setLoading(false);
         },
         error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+          setLoading(false);
+          setErrorText(error);
         }
       );
   };
 
+
+  function sortResults(results, sorting = sortSelector) {
+    switch (sorting) {
+      case 'A-Z':
+        return results.sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        });
+      case 'Z-A':
+        return results.sort((a, b) => {
+          if (a.name < b.name) {
+            return 1;
+          }
+          if (a.name > b.name) {
+            return -1;
+          }
+          return 0;
+        });
+      case 'Male':
+        var maleSortOrder = ['male', 'female', 'hermaphrodite', 'n/a', 'none'];
+        return results.sort((a, b) => {
+          return maleSortOrder.indexOf(a.gender) - maleSortOrder.indexOf(b.gender);
+        });
+      case 'Female':
+        var femaleSortOrder = ['female', 'male', 'hermaphrodite', 'n/a', 'none'];
+        return results.sort((a, b) => {
+          return femaleSortOrder.indexOf(a.gender) - femaleSortOrder.indexOf(b.gender);
+        });
+      default: return results;
+    }
+  }
+
+  function handleSearchTextChange(event) {
+    setSearchText(event.target.value);
+  };
+
+  function handleSortChange(event) {
+    setSortSelector(event.target.value);
+    setCharacters(sortResults(characters, event.target.value))
+
+  };
+
   React.useEffect(() => {
-    loadPeople()
-  }, [])
+    loadPeople();
+  }, []);
 
   return (
     <div className="App">
@@ -44,32 +114,44 @@ function App() {
         </a>
       </nav>
       <div className='search-container'>
-        <TextField className='search-field' label="Search Character" variant="standard" />
-        <Button className='search-button' variant="outlined">Search Character</Button>
+        <TextField className='search-field' label="Search Character" onChange={handleSearchTextChange} value={searchText} variant="standard" />
+        <Button className='search-button' onClick={loadPeople} variant="outlined">Search Character</Button>
       </div>
       <div className='app-content'>
-        <div className='results-container'>
-          <span>Showing x results of y</span>
-          <InputLabel className='filter-label'>Sort by</InputLabel>
-          <Select
-            value={'A'}
-            className='filter'
-            onChange={() => { }}
-          >
-            <MenuItem value={'A'}>A-Z</MenuItem>
-            <MenuItem value={'Z'}>Z-A</MenuItem>
-            <MenuItem value={'M'}>Male</MenuItem>
-            <MenuItem value={'F'}>Female</MenuItem>
-          </Select>
-        </div>
+        {characters?.length > 0 && (
+          <div className='results-container'>
+            <span>Showing {characters?.length} results of {characterCount}</span>
+            <InputLabel className='filter-label'>Sort by</InputLabel>
+            <Select
+              value={sortSelector}
+              className='filter'
+              onChange={handleSortChange}
+            >
+              <MenuItem value={'Unsorted'}>Unsorted</MenuItem>
+              <MenuItem value={'A-Z'}>A-Z</MenuItem>
+              <MenuItem value={'Z-A'}>Z-A</MenuItem>
+              <MenuItem value={'Male'}>Male</MenuItem>
+              <MenuItem value={'Female'}>Female</MenuItem>
+            </Select>
+          </div>
+        )}
         <div className='results-list'>
           <div className='character-card-container'>
-          {characters.map((character, index) => {
-            return (
-             <Card key={index} character={character}/>)
-          })}
+            {loading && !characters?.length && (
+              <span>Loading...</span>
+            )}
+            {!loading && !characters?.length && (
+              <span>No results!</span>
+            )}
+            <span>{errorText}</span>
+            {characters && characters.map((character, index) => {
+              return (
+                <Card key={index} index={index} character={character} />)
+            })}
           </div>
-          <Button className='load-more-button' variant="outlined">Load More</Button>
+          {nextPage && (
+            <Button className='load-more-button' onClick={loadPeople} variant="outlined">Load More</Button>
+          )}
         </div>
       </div>
     </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,15 @@
 import '../App.css';
-import Button from '@mui/material/Button';
+import iconBlack from '../sw-black.png';
+import iconRed from '../sw-red.png';
 import React from 'react';
 
 function Card(props) {
-  const [characters, setCharacters] = React.useState([1]);
-
-  console.log('character', props.character);
-  return (
-    <div className='character-card'>
-        <span className='character-picture'></span>
-        <span className='character-name'>{props.character?.name}</span>
-    </div>
-  );
+    return (
+        <div className='character-card'>
+            <img src={props.index % 2 ? iconRed : iconBlack} height='64px' width='64px' alt='Star wars icon' className='character-picture' />
+            <span className='character-name'>{props.character?.name}</span>
+        </div>
+    );
 }
 
 export default Card;
